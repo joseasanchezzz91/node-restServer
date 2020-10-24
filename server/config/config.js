a30ba4c5d2705e7a340e85bde16a9fc63f0c5957
Jose Sanchez <sanchezzz2391@gmail.com>
2020-10-24 19:52:34 -0300

    variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -6,19 +6,17 @@ process.env.PORT = process.env.PORT || 9090;
 //=========================
 //Entorno
 //=========================
-process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
-
+process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 
 //=========================
 //BD
 //=========================
 
 let url;
-if(process.env.NODE_ENV === 'dev'){
-url = 'mongodb://localhost:27017/cafe'
-}else{
-    url='mongodb+srv://jsanchez:pKUzLKMgflFNfQJm@cluster0.qifjl.mongodb.net/cafe'
+if (process.env.NODE_ENV === "dev") {
+  url = "mongodb://localhost:27017/cafe";
+} else {
+  url = process.env.MONGO_URI;
 }
 
 process.env.URLBD = url;
-
